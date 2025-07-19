0a14ca6f17c007d8e3bdb7d673fe3eacb509c411
Lmzses <ml776566@gmail.com>
2025-07-18 22:56:04 -0500

    Add @vercel/analytics dependency and integrate Analytics component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <router-view />
+  <Analytics />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
 
 <style scoped></style>
